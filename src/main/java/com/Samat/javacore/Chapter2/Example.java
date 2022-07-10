ca9b64d804f5f77b7c4263b6c51c9b135324af72
zhakins <zhakin17@gmail.com>
2022-07-11 00:54:12 +0300

    second push

--- src/main/java/com/Samat/javacore/Chapter2/Example.java
+++ src/main/java/com/Samat/javacore/Chapter2/Example.java
@@ -1,7 +1,5 @@
-
-
 public class Example {
-    public static void main(String[] args){
+    public static void main(String[] args) {
 
     }
 
--- src/main/java/com/Samat/javacore/Chapter2/IfSample.java
+++ src/main/java/com/Samat/javacore/Chapter2/IfSample.java
@@ -1,6 +1,19 @@
-
 public class IfSample {
     public static void main(String[] args) {
+        int x, y;
+        x = 10;
+        y = 20;
+
+        if (x < y) System.out.println("x меньше y");
+
+        x = x * 2;
+        if (x == y) System.out.println("x теперь равно y");
+
+        x = x * 2;
+        if (x > y) System.out.println("x теперь больше y");
+
+        // этот оператор не будет ничего выводить
+        if (x == y) System.out.println("вы ничего не увидите");
 
     }
 }
